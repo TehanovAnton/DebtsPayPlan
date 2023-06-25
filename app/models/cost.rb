ca8a9e511eeea4040f2ac7aac536f3b93357b00d
anton <tehanovanton@gmail.com>
2023-06-25 12:05:01 +0300

    rubocop fixes

--- app/models/cost.rb
+++ app/models/cost.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Cost < ApplicationRecord
-  has_one :debt, dependent: :destroy
+  has_one :debt, dependent: :destro
   belongs_to :costable,
              polymorphic: true
 end
