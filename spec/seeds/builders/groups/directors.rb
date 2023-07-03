30fb0c613709eda8107727646edad1c379dbad8d
anton <tehanovanton@gmail.com>
2023-07-03 16:09:59 +0300

    rubocop fixes

--- spec/seeds/builders/groups/directors.rb
+++ spec/seeds/builders/groups/directors.rb
@@ -1,14 +1,20 @@
-module Seeds::Builders::Groups
-  class GroupDiretor
-    def initialize(group_builder, users, cost)
-      @users = users
-      @costs = cost
-      @group_builder = group_builder
-    end
+# frozen_string_literal: true
 
-    def make_group
-      group_builder.seed_attribute(user_seed.result, attr_name: :users)
-                   .seed_attribute(cost_seed.result, attr_name: :cost_attributes)
+module Seeds
+  module Builders
+    module Groups
+      class GroupDiretor
+        def initialize(group_builder, users, cost)
+          @users = users
+          @costs = cost
+          @group_builder = group_builder
+        end
+
+        def make_group
+          group_builder.seed_attribute(user_seed.result, attr_name: :users)
+                       .seed_attribute(cost_seed.result, attr_name: :cost_attributes)
+        end
+      end
     end
   end
 end
