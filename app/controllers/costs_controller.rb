e57a5c7ddc4aa785247b34b07c3b1631cc72b37d
anton <tehanovanton@gmail.com>
2023-07-15 17:48:44 +0300

    cost pages

--- app/controllers/costs_controller.rb
+++ app/controllers/costs_controller.rb
@@ -1,6 +1,12 @@
 # frozen_string_literal: true
 
 class CostsController < ApplicationController
+  def new
+    @cost = Cost.new
+    @user = User.find(params[:user_id])
+    @group = Group.find(params[:group_id])
+  end
+
   def create
     @group = Group.includes(:users).find(params[:group_id])
     cost_creater = Creaters::CostsCreaters::CostCreater.new(cost_params)
@@ -11,13 +17,39 @@ class CostsController < ApplicationController
     debt_value = cost_creater.cost.cost_value - @group.cost.cost_value
     Debt.create(user: cost_creater.cost.costable, cost: cost_creater.cost, debt_value:)
 
-    group_users_debts_updater = Updaters::DebtsUpdaters::GroupUsersDebtsUpdater.new(
-      @group
-    )
-    group_users_debts_updater.update
+    Updaters::DebtsUpdaters::GroupUsersDebtsUpdater.new(@group).update
+
+    redirect_to group_path(@group)
+  end
+
+  def edit
+    user
+    group
+    cost
+  end
+
+  def update
+    cost.update(cost_params)
+
+    Updaters::CostsUpdaters::GroupCostUpdater.new(group, group.cost).update
+    Updaters::DebtsUpdaters::GroupUsersDebtsUpdater.new(group).update
+
+    redirect_to group_path(group)
   end
 
   private
+
+  def group
+    @group ||= Group.find(params[:group_id])
+  end
+
+  def cost
+    @cost ||= Cost.find(params[:id])
+  end
+
+  def user
+    @user ||= User.find(params[:user_id])
+  end
 
   def cost_params
     params.require(:cost)
