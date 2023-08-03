72653b431cb5ed1457f829cbb553dc9f261d5b1e
anton <tehanovanton@gmail.com>
2023-08-03 11:20:35 +0300

    user devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,14 @@
 # frozen_string_literal: true
+module UserDeviseConfig
+  
+end
 
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
   has_many :group_members,
            as: :group_memberable,
            dependent: :destroy,
