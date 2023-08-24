baa09b263a6ddcd10941d57f1e0c70733552d76e
anton <tehanovanton@gmail.com>
2023-08-24 17:00:46 +0300

    generate group costs channel

---
+++ app/channels/group_costs_channel.rb
@@ -0,0 +1,9 @@
+class GroupCostsChannel < ApplicationCable::Channel
+  def subscribed
+    stream_from
+  end
+
+  def unsubscribed
+    # Any cleanup needed when channel is unsubscribed
+  end
+end
